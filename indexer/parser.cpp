863dbcc7338d82da30f8d98dcc6ea1ad453c375f
Arthur Câmara <camara.arthur@gmail.com>
2015-03-19 15:45:01 -0300

    Beginning creation of parser

---
+++ indexer/parser.cpp
@@ -0,0 +1,9 @@
+//
+//  parser.cpp
+//  
+//
+//  Created by Arthur Câmara on 19/3/15.
+//
+//
+
+#include "parser.h"
---
+++ indexer/parser.h
@@ -0,0 +1,14 @@
+//
+//  parser.h
+//  
+//
+//  Created by Arthur Câmara on 19/3/15.
+//
+//
+
+#ifndef ____parser__
+#define ____parser__
+
+#include <stdio.h>
+
+#endif /* defined(____parser__) */
